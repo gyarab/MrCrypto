302312c10493f69eeda631cbb015354c7b7ea667
davidpavluv <56340125+davidpavluv@users.noreply.github.com>
2019-12-03 20:49:59 +0100

    backend initialized endpoint (historical data) added
    Signed-off-by: davidpavluv <56340125+davidpavluv@users.noreply.github.com>

---
+++ React/backend/app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const fetch = require("node-fetch");
+const fs = require("fs");
+var moment = require("moment");
+const port = 5000;
+
+var historicalData = null;
+
+app.get("/getall", async (req, res, next) => {
+  var data = await get();
+  console.log(data);
+  res.json(data);
+});
+
+//get as much from download (historical) to today as posible
+async function get() {
+  var previous = moment().subtract(4, "days");
+  var formatted = previous.format("YYYY-MM-DD");
+
+  const response = await fetch(
+    "https://api.coindesk.com/v1/bpi/historical/close.json?start=2019-11-27&end=" +
+      formatted
+  );
+
+  var data = await response.json();
+  return data;
+}
+
+//get historical data
+fs.readFile("./src/historicalBitcoinPrice.json", (err, data) => {
+  //to 2019-11-26>
+  if (data) {
+    historicalData = JSON.parse(data);
+  }
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
