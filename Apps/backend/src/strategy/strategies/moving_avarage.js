fc112bb0162ca091f6582ebcd9be91ba2bb76039
davidik-gyarab <55786095+davidik-gyarab@users.noreply.github.com>
2020-01-29 18:34:39 +0100

    MA

--- Apps/backend/src/strategy/strategies/moving_avarage.js
+++ Apps/backend/src/strategy/strategies/moving_avarage.js
@@ -31,73 +31,61 @@ async function start() {
   //hour
 var count = Object.keys(data[3].data).length;
         var cislo = [];
-        var time = [];
-        var curr = [];
         var ma = 0;
           for (var i = 0; i < count; i++) {
           cislo[i] = data[3].data[i][4]
-          time[i] = data[3].data[i][0]
           if (i>=6) {
           for (var p = i-6; p <= i; p++) {
                     ma += cislo[p];
           }
           ma = ma/7;
-          curr = [ma, time[i]];
-          hour.push(curr);
+          hour.push(ma);
           ma = 0;
           }
         }
   //day
   var count = Object.keys(data[2].data).length;
             cislo = [];
-            time = [];
             ma = 0;
             for (var i = 0; i < count; i++) {
             cislo[i] = data[2].data[i][4]
-            time[i] = data[2].data[i][0]
             if (i>=6) {
             for (var p = i-6; p <= i; p++) {
                       ma += cislo[p];
             }
             ma = ma/7;
-            curr = [ma, time[i]];
-            day.push(curr);
+            day.push(ma);
             ma = 0;
             }
           }
   //month
   var count = Object.keys(data[1].data).length;
             cislo = [];
-            time = [];
              ma = 0;
             for (var i = 0; i < count; i++) {
             cislo[i] = data[1].data[i][4]
-            time[i] = data[1].data[i][0]
             if (i>=6) {
             for (var p = i-6; p <= i; p++) {
                       ma += cislo[p];
             }
             ma = ma/7;
-            curr = [ma, time[i]];
-            month.push(curr);
+            month.push(ma);
             ma = 0;
             }
           }
+
   //all
   var count = Object.keys(data[0].data).length;
             cislo = [];
-            time = [];
              ma = 0;
             for (var i = 0; i < count; i++) {
             cislo[i] = data[0].data[i][4]
-            time[i] = data[0].data[i][0]
             if (i>=6) {
             for (var p = i-6; p <= i; p++) {
                       ma += cislo[p];
             }
             ma = ma/7;
-            curr = [ma, time[i]];
-            all.push(curr);
+            all.push(ma);
             ma = 0;
             }
           }
@@ -108,33 +96,8 @@ var count = Object.keys(data[3].data).length;
           { _id: "day", data: day },
           { _id: "hour", data: hour }
         ];
-      const db = client.db(dbName);
-      const c = db.collection(dbCollection);
-
-      //dropping older collection prevents errors
-      try {
-        c.drop((err, ok) => {
-          if (err) console.log("_SKIPPING STRATEGY DROPPING");
-          if (ok) console.log("_OLDER STRATEGY DROPPED");
-        });
-      } catch {}
-
-      c.insertMany(obj, (err, result) => {
-        assert.equal(err, null);
-        console.info("_NEW STRATEGY SAVED");
-        //after saving run updating services
-        updater.start("hour", 60, 60);
-        updater.start("day", 15 * 60, 900);
-        updater.start("month", 6 * 3600, 21600);
-        updater.start("all", 24 * 3600, 86400);
         client.close();
-      });
-    });
-    });
-  } catch (err) {
-    console.error("_STRATEGY HANDLING ERROR: " + err);
-    start();
-  }
-}
-
-module.exports = { start };
+      }
+    );
+  });
+}}
