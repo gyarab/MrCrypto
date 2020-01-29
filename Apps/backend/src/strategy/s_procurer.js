4b4dc21aa56f7a37caeb85e1ac2766b03a36f95e
davidik-gyarab <55786095+davidik-gyarab@users.noreply.github.com>
2020-01-29 21:24:18 +0100

    MA

--- Apps/backend/src/strategy/s_procurer.js
+++ Apps/backend/src/strategy/s_procurer.js
@@ -2,11 +2,10 @@ const moment = require("moment");
 const updater = require("./updater");
 const MongoClient = require("mongodb").MongoClient;
 const assert = require("assert");
-
 const url = "mongodb://localhost:27017",
   dbName = "mrcrypto",
   dbCollection = "strategy";
-
+var ma = require('./strategies/moving_avarage.js');
 async function start() {
   try {
     //get..
@@ -27,87 +26,14 @@ async function start() {
         .toArray((err, data) => {
           if (err) throw err;
 
-//MOVING AVARAGE
-  //hour
-var count = Object.keys(data[3].data).length;
-        var cislo = [];
-        var time = [];
-        var curr = [];
-        var ma = 0;
-          for (var i = 0; i < count; i++) {
-          cislo[i] = data[3].data[i][4]
-          time[i] = data[3].data[i][0]
-          if (i>=6) {
-          for (var p = i-6; p <= i; p++) {
-                    ma += cislo[p];
-          }
-          ma = ma/7;
-          curr = [ma, time[i]];
-          hour.push(curr);
-          ma = 0;
-          }
-        }
-  //day
-  var count = Object.keys(data[2].data).length;
-            cislo = [];
-            time = [];
-            ma = 0;
-            for (var i = 0; i < count; i++) {
-            cislo[i] = data[2].data[i][4]
-            time[i] = data[2].data[i][0]
-            if (i>=6) {
-            for (var p = i-6; p <= i; p++) {
-                      ma += cislo[p];
-            }
-            ma = ma/7;
-            curr = [ma, time[i]];
-            day.push(curr);
-            ma = 0;
-            }
-          }
-  //month
-  var count = Object.keys(data[1].data).length;
-            cislo = [];
-            time = [];
-             ma = 0;
-            for (var i = 0; i < count; i++) {
-            cislo[i] = data[1].data[i][4]
-            time[i] = data[1].data[i][0]
-            if (i>=6) {
-            for (var p = i-6; p <= i; p++) {
-                      ma += cislo[p];
-            }
-            ma = ma/7;
-            curr = [ma, time[i]];
-            month.push(curr);
-            ma = 0;
-            }
-          }
-  //all
-  var count = Object.keys(data[0].data).length;
-            cislo = [];
-            time = [];
-             ma = 0;
-            for (var i = 0; i < count; i++) {
-            cislo[i] = data[0].data[i][4]
-            time[i] = data[0].data[i][0]
-            if (i>=6) {
-            for (var p = i-6; p <= i; p++) {
-                      ma += cislo[p];
-            }
-            ma = ma/7;
-            curr = [ma, time[i]];
-            all.push(curr);
-            ma = 0;
-            }
-          }
-  //SAVE...
         var obj = [
-          { _id: "all", data: all },
-          { _id: "month", data: month },
-          { _id: "day", data: day },
-          { _id: "hour", data: hour }
+          { _id: "movingAvarage", data: ma },
+          { _id: "....", data: .... },
+          { _id: "....", data: .... },
+          { _id: "....", data: .... }
         ];
+
+
       const db = client.db(dbName);
       const c = db.collection(dbCollection);
 
--- Apps/backend/src/strategy/strategies/moving_avarage.js
+++ Apps/backend/src/strategy/strategies/moving_avarage.js
@@ -1,12 +1,11 @@
 const moment = require("moment");
-const updater = require("./updater");
 const MongoClient = require("mongodb").MongoClient;
 const assert = require("assert");
 
 const url = "mongodb://localhost:27017",
   dbName = "mrcrypto",
   dbCollection = "strategy";
-
+  var obj = [];
 async function start() {
   try {
     //get..
@@ -29,75 +28,94 @@ async function start() {
 
 //MOVING AVARAGE
   //hour
-var count = Object.keys(data[3].data).length;
-        var cislo = [];
-        var ma = 0;
-          for (var i = 0; i < count; i++) {
-          cislo[i] = data[3].data[i][4]
-          if (i>=6) {
-          for (var p = i-6; p <= i; p++) {
-                    ma += cislo[p];
-          }
-          ma = ma/7;
-          hour.push(ma);
-          ma = 0;
-          }
-        }
-  //day
-  var count = Object.keys(data[2].data).length;
-            cislo = [];
-            ma = 0;
+  var count = Object.keys(data[3].data).length;
+          var cislo = [];
+          var time = [];
+          var curr = [];
+          var ma = 0;
             for (var i = 0; i < count; i++) {
-            cislo[i] = data[2].data[i][4]
+            cislo[i] = data[3].data[i][4]
+            time[i] = data[3].data[i][0]
             if (i>=6) {
             for (var p = i-6; p <= i; p++) {
                       ma += cislo[p];
             }
             ma = ma/7;
-            day.push(ma);
+            curr = [ma, time[i]];
+            hour.push(curr);
             ma = 0;
             }
           }
-  //month
-  var count = Object.keys(data[1].data).length;
-            cislo = [];
-             ma = 0;
-            for (var i = 0; i < count; i++) {
-            cislo[i] = data[1].data[i][4]
-            if (i>=6) {
-            for (var p = i-6; p <= i; p++) {
-                      ma += cislo[p];
+    //day
+    var count = Object.keys(data[2].data).length;
+              cislo = [];
+              time = [];
+              ma = 0;
+              for (var i = 0; i < count; i++) {
+              cislo[i] = data[2].data[i][4]
+              time[i] = data[2].data[i][0]
+              if (i>=6) {
+              for (var p = i-6; p <= i; p++) {
+                        ma += cislo[p];
+              }
+              ma = ma/7;
+              curr = [ma, time[i]];
+              day.push(curr);
+              ma = 0;
+              }
             }
-            ma = ma/7;
-            month.push(ma);
-            ma = 0;
+    //month
+    var count = Object.keys(data[1].data).length;
+              cislo = [];
+              time = [];
+               ma = 0;
+              for (var i = 0; i < count; i++) {
+              cislo[i] = data[1].data[i][4]
+              time[i] = data[1].data[i][0]
+              if (i>=6) {
+              for (var p = i-6; p <= i; p++) {
+                        ma += cislo[p];
+              }
+              ma = ma/7;
+              curr = [ma, time[i]];
+              month.push(curr);
+              ma = 0;
+              }
             }
-          }
+    //all
+    var count = Object.keys(data[0].data).length;
+              cislo = [];
+              time = [];
+               ma = 0;
+              for (var i = 0; i < count; i++) {
+              cislo[i] = data[0].data[i][4]
+              time[i] = data[0].data[i][0]
+              if (i>=6) {
+              for (var p = i-6; p <= i; p++) {
+                        ma += cislo[p];
+              }
+              ma = ma/7;
+              curr = [ma, time[i]];
+              all.push(curr);
+              ma = 0;
+              }
+            }
+    //SAVE...
+          obj = [
+            { _id: "all", data: all },
+            { _id: "month", data: month },
+            { _id: "day", data: day },
+            { _id: "hour", data: hour }
+          ];
 
-  //all
-  var count = Object.keys(data[0].data).length;
-            cislo = [];
-             ma = 0;
-            for (var i = 0; i < count; i++) {
-            cislo[i] = data[0].data[i][4]
-            if (i>=6) {
-            for (var p = i-6; p <= i; p++) {
-                      ma += cislo[p];
-            }
-            ma = ma/7;
-            all.push(ma);
-            ma = 0;
-            }
-          }
-  //SAVE...
-        var obj = [
-          { _id: "all", data: all },
-          { _id: "month", data: month },
-          { _id: "day", data: day },
-          { _id: "hour", data: hour }
-        ];
         client.close();
       }
     );
   });
-}}
+
+}catch (err) {
+  console.error("_CANDLES HANDLING ERROR: " + err);
+  start();
+}
+}
+console.log(obj);
