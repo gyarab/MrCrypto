a70e461e5a9b404998730f6fc5cacad537469077
davidpavluv <56340125+davidpavluv@users.noreply.github.com>
2020-02-27 00:52:09 +0100

    toolbar styling

--- Apps/mrcrypto1/src/components/ToolBar.js
+++ Apps/mrcrypto1/src/components/ToolBar.js
@@ -2,30 +2,57 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { selectRange } from "../redux/actions/prices";
 import { setActive } from "../redux/actions/indicators";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircle } from "@fortawesome/free-solid-svg-icons";
 import { Row, Col, ToggleButton, ToggleButtonGroup } from "react-bootstrap";
+var colors = require("./indicatorsColors.json");
 
 class ToolBar extends Component {
   render() {
     const handleToggle = indicators => {
-      console.log(indicators);
       this.props.setToggled(indicators);
     };
 
+    let icon = name => {
+      return (
+        <FontAwesomeIcon
+          icon={faCircle}
+          color={colors[name]}
+          style={{ paddingRight: 3 }}
+        />
+      );
+    };
+    let variant = "light";
+
     return (
       <Row className="d-flex justify-content-around">
-        <Col md={3} xs={3} lg={3}>
+        <Col md={6} xs={6} lg={6}>
           <ToggleButtonGroup
             size="sm"
             type="checkbox"
             className="mb-2"
             onChange={handleToggle}
           >
-            <ToggleButton value="sma">SMA</ToggleButton>
-            <ToggleButton value="ema">EMA</ToggleButton>
-            <ToggleButton value="tma">TMA</ToggleButton>
-            <ToggleButton value="wma">WMA</ToggleButton>
-            <ToggleButton value="bob">BOB</ToggleButton>
+            <ToggleButton variant={variant} value="sma">
+              {icon("sma")}SMA
+            </ToggleButton>
+            <ToggleButton variant={variant} value="ema">
+              {icon("ema")}EMA
+            </ToggleButton>
+            <ToggleButton variant={variant} value="tma">
+              {icon("tma")}TMA
+            </ToggleButton>
+            <ToggleButton variant={variant} value="wma">
+              {icon("wma")}WMA
+            </ToggleButton>
+            <ToggleButton variant={variant} value="bob">
+              {icon("bob")}BOB
+            </ToggleButton>
+          </ToggleButtonGroup>
+          <ToggleButtonGroup name="google" toggle size="sm" className="mb-2">
+            <ToggleButton variant={variant} type="checkbox" value="1">
+              {icon("googletrends")}Google Trends
+            </ToggleButton>
           </ToggleButtonGroup>
         </Col>
         <Col md={3} xs={3} lg={3}>
